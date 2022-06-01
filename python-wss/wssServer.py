2d6f844e8ea6a81e98e485ef8edafef6a7bfada0
alessmolt <alessandro.molt@outlook.it>
2022-06-01 15:51:39 +0200

    added writing data to file

--- python-wss/wssServer.py
+++ python-wss/wssServer.py
@@ -10,18 +10,20 @@ from pythonosc.udp_client import SimpleUDPClient
 ip_osc = "127.0.0.1"
 port_osc = 8010
 lista_movimenti  = list()
-
 client_osc = SimpleUDPClient(ip_osc, port_osc)  # Create client
 
 
 loop = asyncio.get_event_loop()
 
-server =  wss.Server(port=8000, useSsl=True, sslCert="python-wss-master/cert/server.crt",
-                     sslKey="python-wss-master/cert/server.key")
+server =  wss.Server(port=8000, useSsl=True, sslCert="python-wss/cert/server.crt",
+                     sslKey="python-wss/cert/server.key")
 
 def onTextMessage(msg, client):
 	print("got message from client:", msg.decode())
 	lista_movimenti.append(msg.decode())
+	f = open("movementdata.txt", "a")
+	f.write(msg.decode()+ "\n")
+	#f.close()
 	if len(lista_movimenti) == 100:
 		print("ENTRATO!!!!!!!")
 		value = sum([float(i) for i in lista_movimenti]) / len(lista_movimenti)
