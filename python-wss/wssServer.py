0b13127f0bdfcf4e4499990a67603f22cc204969
alessmolt <alessandro.molt@outlook.it>
2022-06-09 10:53:29 +0200

    added comments + code cleaning

--- python-wss/wssServer.py
+++ python-wss/wssServer.py
@@ -9,33 +9,37 @@ from pythonosc.udp_client import SimpleUDPClient
 
 ip_osc = "127.0.0.1"
 port_osc = 8010
-lista_movimenti  = list()
-client_osc = SimpleUDPClient(ip_osc, port_osc)  # Create client
+movement_buffer  = list()
+client_osc = SimpleUDPClient(ip_osc, port_osc)  # Create  osc client
+BUFFER_LENGTH = 100		# Number of data on which the mean is calculated
 
 
 loop = asyncio.get_event_loop()
 
+# Create https socket server
 server =  wss.Server(port=8000, useSsl=True, sslCert="python-wss/cert/server.crt",
                      sslKey="python-wss/cert/server.key")
 
+
+# On socket message callback
 def onTextMessage(msg, client):
 	print("got message from client:", msg.decode())
-	lista_movimenti.append(msg.decode())
-	f = open("movementdata.txt", "a")
-	f.write(msg.decode()+ "\n")
-	#f.close()
-	if len(lista_movimenti) == 100:
-		print("ENTRATO!!!!!!!")
-		value = sum([float(i) for i in lista_movimenti]) / len(lista_movimenti)
-		client_osc.send_message("/medias/Gradient_Color/Color/Adjust/Brightness",value)  # Send float message
-		lista_movimenti.clear()
+	movement_buffer.append(msg.decode())
+	if len(movement_buffer) == BUFFER_LENGTH:
+		value = sum([float(i) for i in movement_buffer]) / len(movement_buffer) # Mean of the buffer containing all the user data 
+		print("ENTRATO!!!!!!! --> " + str(value))
+		if value > 20:
+			client_osc.send_message("/cues/selected/columns/6",value)  # Send float message
+		elif value > 10:
+			client_osc.send_message("/cues/selected/columns/5",value)
+		else:
+			client_osc.send_message("/cues/selected/columns/2",value)
+
+		movement_buffer.clear()
 
 
-def onBinaryMessage(msg, client):
-	print("got binary message")
 
 server.setTextHandler(onTextMessage)
-server.setBinaryHandler(onBinaryMessage)
 
 server.start()
 loop.run_forever()
--- sensor-client.js
+++ sensor-client.js
@@ -1,58 +1,54 @@
 let socket = new WebSocket('wss://192.168.1.208:8000');
 
-socket.addEventListener('open', prontoPerMuovere);
+socket.addEventListener('open', readyToStart);
 
-function prontoPerMuovere(event) {
+// Initializes sensor's Event Listeners
+function readyToStart(event) {
   window.addEventListener('deviceorientation', giroscopio);
   window.addEventListener('devicemotion', accellerometro);
 }
 
-const lista_dati = new Array();
+const buffer_dati = new Array();
+const MAX_ROTATION = 1800; // Rotation rate value corresponding to a multiplying factor of 1.5 of the acceleration
+const BUFFER_LENGTH = 20; // Number of single user data to be stored in the buffer
 
 /*
 ACCELERATION: An object giving the acceleration of the device on the three axis X, Y and Z. Acceleration is expressed in m/s²
-
 ROTATION RATE: An object giving the rate of change of the device's orientation on the three orientation axis alpha, beta and gamma. Rotation rate is expressed in degrees per seconds.
-
 INTERVAL: A number representing the interval of time, in milliseconds, at which data is obtained from the device.
 */
-
 function accellerometro(event) {
-  lista_dati.push(
-    Math.abs(event.rotationRate.alpha) +
+  let rotation_rate = //rescaled value between 1 and 1.5 (according to the value of MAX_ROTATION)
+    ((Math.abs(event.rotationRate.alpha) +
       Math.abs(event.rotationRate.beta) +
-      Math.abs(event.rotationRate.gamma)
-  );
+      Math.abs(event.rotationRate.gamma)) *
+      (1.5 - 1)) /
+      MAX_ROTATION +
+    1;
 
-  console.log(lista_dati.length);
-  if (lista_dati.length === 20) {
-    //ogni quanti messaggi invia i dati mediati al server
-    let sum = lista_dati.reduce((a, b) => a + b, 0);
-    socket.send(((sum / lista_dati.length) * (1 + 1)) / 1500 - 1); // media movimento singolo utente
-    lista_dati.length = 0;
+  buffer_dati.push(
+    Math.sqrt(
+      Math.pow(event.acceleration.x, 2) +
+        Math.pow(event.acceleration.y, 2) +
+        Math.pow(event.acceleration.z, 2)
+    ) * rotation_rate
+  );
+  if (buffer_dati.length === BUFFER_LENGTH) {
+    let sum_acc = buffer_dati.reduce((a, b) => a + b, 0);
+    socket.send(sum_acc / buffer_dati.length); //sends to the server the mean of the buffer data
+    buffer_dati.length = 0;
   }
 }
 
 /*
 ABSOLUTE = true if the orientation data in instanceOfDeviceOrientationEvent is provided as the difference between the Earth's coordinate frame and the device's coordinate frame
 false if the orientation data is being provided in reference to some arbitrary, device-determined coordinate frame.
-
 ALPHA  = Rotation around the z axis -- that is, twisting the device -- causes the alpha rotation angle to change:
 The alpha angle is 0° when top of the device is pointed directly toward the Earth's north pole, and increases as the device is rotated toward the left.
-
 BETA = Rotation around the x axis -- that is, tipping the device away from or toward the user -- causes the beta rotation angle to change:The beta angle is 0° when the device's top and bottom are the same distance from the Earth's surface; it increases toward 180° as the device is tipped forward toward the user, and it decreases toward -180° as the device is tipped backward away from the user.
-
 GAMMA = Rotation around the y axis -- that is, tilting the device toward the left or right -- causes the gamma rotation angle to change:The gamma angle is 0° when the device's left and right sides are the same distance from the surface of the Earth, and increases toward 90° as the device is tipped toward the right, and toward -90° as the device is tipped toward the left.
 */
-
 function giroscopio(evento) {
   document.getElementById('body').style.backgroundColor =
     'rgb(' + evento.alpha + ',' + evento.beta + ',' + evento.gamma + ')';
-  // socket.send(
-  //   JSON.stringify({
-  //     alpha: evento.alpha,
-  //     beta: evento.beta,
-  //     gamma: evento.gamma,
-  //   })
-  // );
 }
